e862f204d2db4398e99a5bd706f9aa10123142bf
[Omar Khaled Mohy] <[omarkhaledm21@gmail.com]>
2021-11-16 18:47:31 +0200

    Modified UML Classes

--- src/Admin.java
+++ src/Admin.java
@@ -1,3 +1,9 @@
+
 public class Admin extends User{
+
+
+    public Admin(String username,String password,String email,String mobileNumber){
+        super(username,password,email, mobileNumber);
+    }
 
 }
--- src/System.java
+++ src/System.java
@@ -1,2 +1,12 @@
 public class System {
+    private static System system;
+
+    private System(){}
+
+    public static System getSystem(){
+        if(system==null){
+            system = new System();
+        }
+        return system;
+    }
 }
