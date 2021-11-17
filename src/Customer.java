9bafa55bf59ca03d5e81dc2a0f1bb2ba6f97665b
[Omar Khaled Mohy] <[omarkhaledm21@gmail.com]>
2021-11-17 21:55:08 +0200

    checkAverageRating(), rateRide() methods added.

--- src/Customer.java
+++ src/Customer.java
@@ -31,7 +31,34 @@ public class Customer extends User {
         this.ride = new Ride(source,destination);
 
         system.newRideNotify(this.ride);
+    }
+
+    public void rateRide(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your rating 1-5: ");
+        int rating = input.nextInt();
+        this.ride.setRating(rating);
+        System.out.println("You rate this ride "+rating+" out of 5!");
+    }
+
+    public void checkAverageRating(){
+        System.out.println("Average rating: "+this.ride.getDriver().getAverageRating());
+    }
+
+    public void checkOffers(){
+        System.out.println("Checking for available offers....");
+        ArrayList<Offer> offers = this.ride.getPriceOffers();
+        if(offers != null){
+            for(int i = 0; i<offers.size(); i++){
+                System.out.println(i+". "+offers.get(i).toString());
+                System.out.println("Average Rating: "+offers.get(i).getDriver().getAverageRating());
+            }
 
 
+
+
+        }else{
+            System.out.println("No offers for this ride yet!");
+        }
     }
 }
